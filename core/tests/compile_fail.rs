f0c7e9be02c27fe5b0bec3d6eb9438f2573aa122
Dean 利迪恩 <dean@bitping.com>
2026-03-05 15:09:32 +0800

    unexpected configs

--- core/tests/compile_fail.rs
+++ core/tests/compile_fail.rs
@@ -1,8 +1,5 @@
 #[test]
 fn compile_fail_tests() {
-    // Set check-cfg for solana target_os so trybuild doesn't warn about it
-    std::env::set_var("RUSTFLAGS", "--check-cfg=cfg(target_os,values(\"solana\"))");
-
     let t = trybuild::TestCases::new();
     t.compile_fail("tests/compile_fail/*.rs");
 }
